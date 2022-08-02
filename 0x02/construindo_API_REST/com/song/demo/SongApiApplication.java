92a533b5daad13a07b7c07c04e9d7ad5858c1b9e
miguelmoraisjr <miguelj@ciandt.com>
2022-08-02 11:34:26 -0300

    Empacotamento

--- 0x02/construindo_API_REST/com/song/demo/SongApiApplication.java
+++ 0x02/construindo_API_REST/com/song/demo/SongApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConstruindoApiRestApplication {
+public class SongApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConstruindoApiRestApplication.class, args);
+		SpringApplication.run(SongApiApplication.class, args);
 	}
 
 }
